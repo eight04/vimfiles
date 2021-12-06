4d63000e5031ea96f787ad09c62f34487f2ac182
eight04 <eight04@gmail.com>
2021-12-06 18:15:52 +0800

    Update submodules

--- add_submodule.py
+++ add_submodule.py
@@ -1,6 +1,10 @@
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 from subprocess import run
 import re
+
+def posix_path(p):
+    # https://github.com/git-for-windows/git/issues/3575
+    return str(PurePosixPath(p))
 
 root = Path("./pack/plugins/start")
 for p in root.iterdir():
@@ -8,4 +12,6 @@ for p in root.iterdir():
     host = re.search(r'https://\S+', r.stdout.decode("utf8")).group()
     r = run("git status", shell=True, cwd=p, capture_output=True)
     branch = re.search(r'On branch (\S+)', r.stdout.decode("utf8")).group(1)
-    run(["git", "submodule", "add", "-b", branch, host, str(p)], shell=True)
+    #  run(["git", "submodule", "add", "-b", branch, host, str(p)], shell=True)
+    run(["git", "submodule", "set-branch", "-b", branch, posix_path(p)], shell=True, check=True)
+    run(["git", "submodule", "set-url", posix_path(p), host], shell=True, check=True)
